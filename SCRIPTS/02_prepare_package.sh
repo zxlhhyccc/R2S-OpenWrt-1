6565f8375bc2ec8a7340f4fc05e6b625d205ee47
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-06-26 15:26:59 +0800

    add: luci-app-oled @NateLol

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -77,6 +77,8 @@ svn co https://github.com/openwrt/packages/branches/openwrt-18.06/net/ddns-scrip
 svn co https://github.com/openwrt/luci/branches/openwrt-18.06/applications/luci-app-ddns feeds/luci/applications/luci-app-ddns
 #Pandownload
 svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lean/pandownload-fake-server package/lean/pandownload-fake-server
+#oled
+git clone -b master --single-branch https://github.com/NateLol/luci-app-oled package/new/luci-app-oled
 #网易云解锁
 git clone -b master --single-branch https://github.com/project-openwrt/luci-app-unblockneteasemusic package/new/UnblockNeteaseMusic
 #定时重启
@@ -166,8 +168,8 @@ mkdir package/base-files/files/usr/bin
 cp -f ../PATCH/irq_optimize.sh package/base-files/files/usr/bin/irq_optimize.sh
 cp -f ../PATCH/irq_optimize package/base-files/files/etc/init.d/irq_optimize
 #i2c_oled
-cp -f ../I2C/i2c_ssd package/base-files/files/usr/bin/i2c_ssd
-cp -f ../I2C/OLED_R2S package/base-files/files/etc/init.d/OLED_R2S
+#cp -f ../I2C/i2c_ssd package/base-files/files/usr/bin/i2c_ssd
+#cp -f ../I2C/OLED_R2S package/base-files/files/etc/init.d/OLED_R2S
 #删除已有配置
 rm -rf .config
 #授予权限

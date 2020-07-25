fad554dd07b007e74d4f5ad5fcf611723eb56978
KaneGreen <737445366KG@Gmail.com>
2020-07-25 18:47:02 +0800

    update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -13,5 +13,5 @@ cd ..
 # clone others' source for some packages
 $(which wget) --https-only --retry-connrefused https://github.com/Lienol/openwrt/archive/dev-19.07.tar.gz
 tar xf dev-19.07.tar.gz
-rm  -f  dev-19.07.tar.gz
+rm  -f dev-19.07.tar.gz
 exit 0

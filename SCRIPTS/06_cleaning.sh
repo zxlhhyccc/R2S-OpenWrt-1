af4afde893be0f62eff2d388352f2c682130f379
KaneGreen <737445366KG@Gmail.com>
2020-08-08 15:29:27 +0800

    Revert "update 06_cleaning.sh" (commit 0109edebbff43e41caeb6d554854c152d3c4b1e7)

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /bin/ls -AFhlt
 rm -rf `/bin/ls | grep -v -E '(squashfs|manifest)'`
+gzip -d *.gz
 gzip --best --keep *.img
 sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
 md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash

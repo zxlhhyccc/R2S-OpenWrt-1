952a067bceba1ccacbba54bd910778111aafa35d
KaneGreen <737445366KG@Gmail.com>
2020-08-12 11:20:35 +0800

    experimentl: kernel 5.4.58

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -3,6 +3,10 @@ set -x
 set -e
 alias wget="$(which wget) --https-only --retry-connrefused"
 
+MYNOWKERNEL=$(grep 'LINUX_VERSION-5.4' kernel-version.mk)
+[ ${MYNOWKERNEL##*.} -lt 58 ] && patch -p1 < ../PATCH/kernel58.patch
+unset MYNOWKERNEL
+
 ### 1. 准备工作 ###
 # 使用19.07的feed源
 rm -f ./feeds.conf.default

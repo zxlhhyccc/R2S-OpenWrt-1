c83cae7595e02452b1242510effd44642997128a
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-10-02 12:00:28 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -163,7 +163,10 @@ svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/p
 #oled
 git clone -b master --single-branch https://github.com/NateLol/luci-app-oled package/new/luci-app-oled
 #网易云解锁
-git clone -b master --single-branch https://github.com/project-openwrt/luci-app-unblockneteasemusic package/new/UnblockNeteaseMusic
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic package/new/UnblockNeteaseMusic
+pushd package/new/UnblockNeteaseMusic
+git checkout v2.8-6
+popd
 #定时重启
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/lean/luci-app-autoreboot
 #argon主题

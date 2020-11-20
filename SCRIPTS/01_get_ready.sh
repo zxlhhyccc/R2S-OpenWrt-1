a522e735acf7ad4534800195651f928d89750d6c
Kane <KaneGreen@users.noreply.github.com>
2020-11-20 23:40:49 +0800

    Update 01_get_ready.sh
    clone less

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -3,8 +3,8 @@ set -e
 # use master branch
 git clone --single-branch -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
 # download others' source for some packages
-git clone --single-branch https://github.com/Lienol/openwrt.git      openwrt-lienol
-git clone --single-branch https://github.com/Lienol/openwrt-packages packages-lienol
-git clone --single-branch https://github.com/Lienol/openwrt-luci     luci-lienol
+git clone --depth 1 https://github.com/Lienol/openwrt.git      openwrt-lienol
+git clone --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
+git clone --depth 1 https://github.com/Lienol/openwrt-luci     luci-lienol
 rm -rf ./*-lienol/.git
 exit 0

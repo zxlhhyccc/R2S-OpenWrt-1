86dd8a79b398d8536f2e1e3b69b6ed3a3500cb59
KaneGreen <737445366KG@Gmail.com>
2020-11-24 17:06:27 +0800

    add new SSRP PR

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -168,6 +168,7 @@ svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook         
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/ssocks           package/new/ssocks
 # manually merge SSRP PRs
 pushd package/lean
+wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/239.patch | patch -p1
 wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/218.patch | patch -p1
 popd
 # OpenClash
